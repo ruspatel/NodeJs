aaa0540f6f656a4c90b05b16b34997444185f59f
Rushil Patel <ra8patel@edu.uwaterloo.ca>
2019-11-17 14:22:52 -0500

    printing current temp and rain probability

--- node-course/weather-app/app.js
+++ node-course/weather-app/app.js
@@ -3,10 +3,12 @@ const request = require('request')
 //api url
 const url = 'https://api.darksky.net/forecast/bbd429de0bff23627c60f113dc9ed810/37.8267,-122.4233'
 
-request({url: url}, (error, response) => {
+
+request({url: url, json : true}, (error, response) => {
     //console.log(response)
     //parsing the data to convert JSON to Javascript object
-    const data = JSON.parse(response.body)
-    console.log(data.currently)
-    
+    //json is set to true so no need for: const data = JSON.parse(response.body)
+    const currentTemp = response.body.currently.temperature
+    const precipChance = response.body.currently.precipProbability
+    console.log("It is currently "+currentTemp+", with a "+precipChance+" chance of rain.")
 })
