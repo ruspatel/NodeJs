093cbdee73b8f15b8324a8c0e3cd30253faa68f2
Rushil Patel <ra8patel@edu.uwaterloo.ca>
2019-11-17 14:33:24 -0500

    updated location and units

--- node-course/weather-app/app.js
+++ node-course/weather-app/app.js
@@ -1,7 +1,7 @@
 const request = require('request')
 
 //api url
-const url = 'https://api.darksky.net/forecast/bbd429de0bff23627c60f113dc9ed810/37.8267,-122.4233'
+const url = 'https://api.darksky.net/forecast/bbd429de0bff23627c60f113dc9ed810/43.7315, 79.7624?units=si'
 
 
 request({url: url, json : true}, (error, response) => {
@@ -10,5 +10,5 @@ request({url: url, json : true}, (error, response) => {
     //json is set to true so no need for: const data = JSON.parse(response.body)
     const currentTemp = response.body.currently.temperature
     const precipChance = response.body.currently.precipProbability
-    console.log("It is currently "+currentTemp+", with a "+precipChance+" chance of rain.")
+    console.log("It is currently "+currentTemp+", with a "+precipChance+" chance of precipiation (snow or rain).")
 })
