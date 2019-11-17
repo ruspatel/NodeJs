0c26747c753bcdf5da7a6f31b459d65c178e01ae
Rushil Patel <ra8patel@edu.uwaterloo.ca>
2019-11-17 16:59:43 -0500

    added some error handling

--- node-course/weather-app/app.js
+++ node-course/weather-app/app.js
@@ -1,26 +1,38 @@
 const request = require('request')
 
 //api url
-const url = 'https://api.darksky.net/forecast/bbd429de0bff23627c60f113dc9ed810/43.7315, 79.7624?units=si'
-
+const url = 'https://api.darksky.net/forecast/bbd429de0bff23627c60f113dc9ed810/43.7315,-79.7624?units=si'
 
 request({url: url, json : true}, (error, response) => {
     //console.log(response)
     //parsing the data to convert JSON to Javascript object
     //json is set to true so no need for: const data = JSON.parse(response.body)
-    const currentTemp = response.body.currently.temperature
-    const precipChance = response.body.currently.precipProbability
-    console.log("It is currently "+currentTemp+", with a "+precipChance+" chance of precipiation (snow or rain).")
+
+    if(error){
+        console.log('Unable to connect to network!')
+    }else if(response.body.error){
+        console.log('Unable to find location!')
+    }else{
+        const currentTemp = response.body.currently.temperature
+        const precipChance = response.body.currently.precipProbability
+        console.log("It is currently "+currentTemp+", with a "+precipChance+" chance of precipiation (snow or rain).")
+    }
 })
 
-//Geocoding: converting address to a latitude and longitude address
-//mapbox access token: pk.eyJ1IjoicnVzcGF0ZWwiLCJhIjoiY2szM2V1MWVlMHUyNDNvazNkNXhsb2JrYyJ9.UDAYGOlItoCYAyr0n3LRFA
+// Geocoding: converting address to a latitude and longitude address
+// mapbox access token: pk.eyJ1IjoicnVzcGF0ZWwiLCJhIjoiY2szM2V1MWVlMHUyNDNvazNkNXhsb2JrYyJ9.UDAYGOlItoCYAyr0n3LRFA
 
-const geocode_url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/-73.989,40.733.json?access_token=pk.eyJ1IjoicnVzcGF0ZWwiLCJhIjoiY2szM2V1MWVlMHUyNDNvazNkNXhsb2JrYyJ9.UDAYGOlItoCYAyr0n3LRFA'
+const geocode_url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Brampton,Ontario.json?access_token=pk.eyJ1IjoicnVzcGF0ZWwiLCJhIjoiY2szM2V1MWVlMHUyNDNvazNkNXhsb2JrYyJ9.UDAYGOlItoCYAyr0n3LRFA'
 
 request({url: geocode_url, json: true}, (error, response) => {
-    const latitude = response.body.features[0].center[1]
-    const longitude = response.body.features[0].center[0 ]
-    console.log("The latitude and longitude are "+longitude+", "+latitude+" .")
+    if(error){
+        console.log("Unable to connect to network!")
+    }else if(response.body.features.length === 0){
+        console.log("Unable to find location!")
+    }else{
+        const latitude = response.body.features[0].center[1]
+        const longitude = response.body.features[0].center[0]
+        console.log("The latitude and longitude are "+latitude+", "+longitude+" .")
+    }
 }
 )
