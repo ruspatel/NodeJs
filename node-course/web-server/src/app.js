7b3965e0db8a647330b70fbcf0b0ecb713bddf0c
Rushil Patel <ra8patel@edu.uwaterloo.ca>
2019-11-18 23:15:19 -0500

    added web-server directory

---
+++ node-course/web-server/src/app.js
@@ -0,0 +1,18 @@
+//express is a function
+const express = require('express')
+
+const app = express()
+
+
+app.get('', (req, res) => {
+    res.send('Hello Express!')
+})
+
+//app.com
+//app.com/help
+//app.com/about
+
+//start server, listens on a specific port
+app.listen(3000, () => {
+    console.log('Server is up on port 3000.')
+})
