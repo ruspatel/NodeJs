cc00230fdf67ff60ee61cd69166fdb7d9f6d420f
Rushil Patel <ra8patel@edu.uwaterloo.ca>
2019-11-19 19:55:03 -0500

    adde 4 routes

--- node-course/web-server/src/app.js
+++ node-course/web-server/src/app.js
@@ -8,6 +8,18 @@ app.get('', (req, res) => {
     res.send('Hello Express!')
 })
 
+app.get('/help', (req, res) => {
+    res.send('Help Page!')
+})
+
+app.get('/about', (req, res) => {
+    res.send('About!')
+})
+
+app.get('/weather', (req, res) => {
+    res.send('Weather')
+})
+
 //app.com
 //app.com/help
 //app.com/about
